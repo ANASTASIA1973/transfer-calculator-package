ec6007df282c3838cb840b58c5a49852786171fb
Anastasia <ankokkinidou5@gmail.com>
2025-06-15 22:10:55 +0200

    Revert static export—enable SSR routing

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // hier darfst du andere Optionen stehen lassen, aber kein `output: 'export'`
 };
 
 module.exports = nextConfig;
